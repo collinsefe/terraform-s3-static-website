907037616331136b73419c2ccb4f395c1c3cd4ea
Collins Orighose <collinsefe@yahoo.com>
2025-02-03 23:52:04 +0000

    update s3 name

--- policy.tf
+++ policy.tf
@@ -1,24 +1,24 @@
-resource "aws_s3_bucket_policy" "that" {
-  bucket = aws_s3_bucket.foo.id
-  policy = data.aws_iam_policy_document.static_website.json
-}
+# resource "aws_s3_bucket_policy" "that" {
+#   bucket = aws_s3_bucket.foo.id
+#   policy = data.aws_iam_policy_document.static_website.json
+# }
 
-data "aws_iam_policy_document" "static_website" {
-  statement {
-    effect = "Allow"
+# data "aws_iam_policy_document" "static_website" {
+#   statement {
+#     effect = "Allow"
 
-    actions = [
-      "s3:GetObject"
-    ]
+#     actions = [
+#       "s3:GetObject"
+#     ]
 
-    resources = [
-      aws_s3_bucket.foo.arn,
-      "${aws_s3_bucket.foo.arn}/*",
-    ]
+#     resources = [
+#       aws_s3_bucket.foo.arn,
+#       "${aws_s3_bucket.foo.arn}/*",
+#     ]
 
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-  }
-}
+#     principals {
+#       type        = "AWS"
+#       identifiers = ["*"]
+#     }
+#   }
+# }
